f31323a0cecade99b2bef78478a665b7db19c61a
Johnisonn <151909152+Johnisonn@users.noreply.github.com>
2025-01-03 17:50:28 +0800

    Update main.py

--- function/main.py
+++ function/main.py
@@ -1,9 +1,9 @@
-from function.config import response_time, is_response_test, source_urls, is_dump_remove, is_match_template
-from function.duplicate_removel import remove_dump_urls
-from function.fetch import fetch_chs, fetch_chs_name
-from function.match import match_chs
-from function.response_test import test_resp_multi_thread
-from function.save_as import save_chs_as_txt, save_chs_as_m3u, save_names_as_txt
+from config import response_time, is_response_test, source_urls, is_dump_remove, is_match_template
+from duplicate_removel import remove_dump_urls
+from fetch import fetch_chs, fetch_chs_name
+from match import match_chs
+from response_test import test_resp_multi_thread
+from save_as import save_chs_as_txt, save_chs_as_m3u, save_names_as_txt
 
 if __name__ == '__main__':
     # chs = fetch_chs(source_urls)
